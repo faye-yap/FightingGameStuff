114860398ee3ecf6ccc02be23e97871b5d5eed98
khliyh <khlyih12345@gmail.com>
2022-07-19 15:43:48 +0800

    updated playercontroller, added constants classes

---
+++ Assets/Scripts/Constants/CharacterConstants.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterConstants : MonoBehaviour {
+    //A normals
+    public int neutralAStartup = 10;
+    public int neutralAActive = 10;
+    public int neutralArecovery = 10;
+    public int crouchingAStartup = 10;
+    public int crouchingAActive = 10;
+    public int crouchingArecovery = 10;    
+    public int jumpingAStartup = 10;
+    public int jumpingAActive = 10;
+    public int jumpingArecovery = 10;
+
+    //B Normals
+    public int neutralBStartup = 10;
+    public int neutralBActive = 10;
+    public int neutralBrecovery = 10;
+    public int crouchingBStartup = 10;
+    public int crouchingBActive = 10;
+    public int crouchingBrecovery = 10;    
+    public int jumpingBStartup = 10;
+    public int jumpingBActive = 10;
+    public int jumpingBrecovery = 10;
+    public int downForwardBStartup = 10;
+    public int downForwardBActive = 10;
+    public int downForwardBRecovery = 10;
+
+    //Specials
+    public int neutralSStartup = 10;
+    public int neutralSActive = 10;
+    public int neutralSrecovery = 10;
+    public int crouchingSStartup = 10;
+    public int crouchingSActive = 10;
+    public int crouchingSrecovery = 10;    
+    public int forwardSStartup = 10;
+    public int forwardSActive = 10;
+    public int forwardSrecovery = 10;
+}
---
+++ Assets/Scripts/Constants/PawnConstants.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnConstants : CharacterConstants
+{
+    void Awake(){
+        neutralAStartup = 10;
+        neutralAActive = 10;
+        neutralArecovery = 10;
+        crouchingAStartup = 10;
+        crouchingAActive = 10;
+        crouchingArecovery = 10;    
+        jumpingAStartup = 10;
+        jumpingAActive = 10;
+        jumpingArecovery = 10;
+        neutralBStartup = 10;
+        neutralBActive = 10;
+        neutralBrecovery = 10;
+        crouchingBStartup = 10;
+        crouchingBActive = 10;
+        crouchingBrecovery = 10;    
+        jumpingBStartup = 10;
+        jumpingBActive = 10;
+        jumpingBrecovery = 10;
+        downForwardBStartup = 10;
+        downForwardBActive = 10;
+        downForwardBRecovery = 10;
+        neutralSStartup = 10;
+        neutralSActive = 10;
+        neutralSrecovery = 10;
+        crouchingSStartup = 10;
+        crouchingSActive = 10;
+        crouchingSrecovery = 10;    
+        forwardSStartup = 10;
+        forwardSActive = 10;
+        forwardSrecovery = 10;
+    }
+}
