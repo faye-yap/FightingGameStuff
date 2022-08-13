1013450e868e35a2d156ab7128da0fad129f9d93
khliyh <73923790+khliyh@users.noreply.github.com>
2022-08-14 01:42:47 +0800

    .

--- Assets/Scripts/InstantiateProjectile.cs
+++ Assets/Scripts/InstantiateProjectile.cs
@@ -9,8 +9,9 @@ public class InstantiateProjectile : MonoBehaviour
     public float initialVelocityMultiplier = 1.25f;
     void InstantiateProj(){
         GameObject projectile = Instantiate(projectilePrefab,transform.position, Quaternion.identity);
-        projectile.transform.SetParent(transform);
+        
         projectile.transform.localScale = new Vector3(1,1,1);
+        projectile.tag = transform.tag;
         Rigidbody2D body = projectile.GetComponent<Rigidbody2D>();
         body.velocity = Vector2.right * initialVelocityMultiplier * GetComponentInParent<PlayerController>().transform.localScale * 5;
     }
