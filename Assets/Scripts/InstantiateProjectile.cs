bab1c6b4e38dd917c5aa00e83a654f7f7e8c70c7
khliyh <khlyih12345@gmail.com>
2022-08-15 07:42:23 +0800

    bishop 3b fix

--- Assets/Scripts/InstantiateProjectile.cs
+++ Assets/Scripts/InstantiateProjectile.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class InstantiateProjectile : MonoBehaviour
 {
-
+    private GameObject projectile;
     public GameObject projectilePrefab;
     public float initialVelocityMultiplier = 1.25f;
     void InstantiateProj(){
         Debug.Log(transform.parent.name);
         Vector3 initVector = new Vector3(transform.parent.position.x + transform.parent.localScale.x * projectilePrefab.transform.position.x,transform.parent.position.y + projectilePrefab.transform.position.y,transform.parent.position.z + projectilePrefab.transform.position.z);
-        GameObject projectile = Instantiate(projectilePrefab,initVector, Quaternion.identity);
+        projectile = Instantiate(projectilePrefab,initVector, Quaternion.identity);
         
         projectile.tag = transform.tag;
+        Rigidbody2D body = projectile.GetComponent<Rigidbody2D>();
+        
+        StartCoroutine(delaySetVelocity());
+        
+    }
+
+    IEnumerator delaySetVelocity(){
+        yield return null;
         Rigidbody2D body = projectile.GetComponent<Rigidbody2D>();
         
         body.velocity = Vector2.right * initialVelocityMultiplier * GetComponentInParent<PlayerController>().transform.localScale.x * 5.0f;
